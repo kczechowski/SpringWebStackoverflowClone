442d7de1307024f05b05ca09227d9a4a3795a858
kczechowski <karolcz97@gmail.com>
2019-12-16 21:24:26 +0100

    Added requests mapping to UserController

--- src/main/java/com/kczechowski/stackclone/controllers/UserController.java
+++ src/main/java/com/kczechowski/stackclone/controllers/UserController.java
@@ -3,9 +3,7 @@ package com.kczechowski.stackclone.controllers;
 import com.kczechowski.stackclone.entities.User;
 import com.kczechowski.stackclone.repositories.UserRepository;
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.ResponseBody;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
 
 import java.util.List;
 
@@ -18,5 +16,34 @@ public class UserController {
     @GetMapping("/users")
     List<User> all() {
         return userRepository.findAll();
+    }
+
+    @PostMapping("/users")
+    User newUser(@RequestBody User user) {
+        return userRepository.save(user);
+    }
+
+    @GetMapping("/users/{id}")
+    User one(@PathVariable int id) {
+
+        return userRepository.findById(id)
+                .orElseThrow(() -> new RuntimeException());
+    }
+
+
+    @PutMapping("/users/{id}")
+    User replaceUser(@RequestBody User user, @PathVariable int id) {
+
+        return userRepository.findById(id)
+                .map(u -> {
+                    u.setNickname(user.getNickname());
+                    u.setEmail(user.getEmail());
+                    return userRepository.save(u);
+                }).orElseThrow(() -> new RuntimeException());
+    }
+
+    @DeleteMapping("/users/{id}")
+    void deleteUser(@PathVariable int id) {
+        userRepository.deleteById(id);
     }
 }
